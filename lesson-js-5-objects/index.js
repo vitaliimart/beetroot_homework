0993df33923d48d93c58620911b46b58b3e0b6a8
vitaliimart <vitalii.martych@gmail.com>
2022-10-23 03:11:34 +0300

    edit lesson-5-js

--- lesson-js-5-objects/index.js
+++ lesson-js-5-objects/index.js
@@ -6,7 +6,7 @@ const car = {
   middleSpeed: 140,
   volumeFuel: 40,
   consumptionFuel: 14,
-  driver: prompt("Введіть будь-яке ім'я водія"),
+  // driver: prompt("Введіть будь-яке ім'я водія"),
 };
 
 // Завдання 1 - Метод, який виводить на екран інформацію про автомобіль
@@ -90,67 +90,38 @@ let myTime = {
     myTime.seconds
   );
 }),
-  // Задача 2 - Функція, що перераховує базовий час, якщо я ввів секунди
-  // Код великий, тому що я вирішив додати трохи краси у вигляді "0"
-  (myTime.showChangeSecond = () => {
-    let tempSeconds = Math.abs(
-      +prompt('Введіть будь-яку кількість секунд числом')
+  myTime.showBaseTime();
+// Задача 2 - Функція, що перераховує базовий час, якщо я ввів секунди
+myTime.showChangeSecond = () => {
+  let tempSeconds = Math.abs(
+    +prompt('Введіть будь-яку кількість секунд числом')
+  );
+
+  if (tempSeconds < 60) {
+    myTime.seconds += tempSeconds;
+    console.log(
+      `Переданий час (у секундах) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
     );
-
-    if (tempSeconds < 60) {
-      myTime.seconds += tempSeconds;
-
-      if (myTime.hours == 0 || myTime.hours < 10) {
-        myTime.hours = String(0) + myTime.hours;
-      }
-      if (myTime.minutes == 0 || myTime.minutes < 10) {
-        myTime.minutes = String(0) + myTime.minutes;
-      }
-      if (myTime.seconds < 10) {
-        myTime.seconds = String(0) + myTime.seconds;
-      }
-      console.log(
-        `Переданий час (у секундах) ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
-      );
-    } else if (tempSeconds >= 60 && tempSeconds < 3600) {
-      myTime.minutes += Math.floor(tempSeconds / 60);
-      myTime.seconds += Math.floor(tempSeconds % 60);
-      if (myTime.hours == 0 || myTime.hours < 10) {
-        myTime.hours = String(0) + myTime.hours;
-      }
-      if (myTime.minutes < 10) {
-        myTime.minutes = String(0) + myTime.minutes;
-      }
-      if (myTime.seconds < 10) {
-        myTime.seconds = String(0) + myTime.seconds;
-      }
-      console.log(
-        `Переданий час ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
-      );
-    } else if (tempSeconds >= 3600 && tempSeconds <= 86400) {
-      myTime.hours += Math.floor(tempSeconds / 3600);
-      myTime.minutes += Math.floor(Math.floor(tempSeconds % 3600) / 60);
-      myTime.seconds += Math.floor(Math.floor(tempSeconds % 3600) % 60);
-      if (myTime.hours < 10) {
-        myTime.hours = String(0) + myTime.hours;
-      }
-      if (myTime.minutes < 10) {
-        myTime.minutes = String(0) + myTime.minutes;
-      }
-      if (myTime.seconds < 10) {
-        myTime.seconds = String(0) + myTime.seconds;
-      }
-      console.log(
-        `Переданий час (через зміну секунд): ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
-      );
-    } else {
-      console.log('Ви ввели проміжок часу більше, ніж 24 години');
-    }
-  });
-// myTime.showChangeSecond();
+  } else if (tempSeconds >= 60 && tempSeconds < 3600) {
+    myTime.minutes += Math.floor(tempSeconds / 60);
+    myTime.seconds += Math.floor(tempSeconds % 60);
+    console.log(
+      `Переданий час (через зміну секунд) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
+    );
+  } else if (tempSeconds >= 3600 && tempSeconds <= 86400) {
+    myTime.hours += Math.floor(tempSeconds / 3600);
+    myTime.minutes += Math.floor(Math.floor(tempSeconds % 3600) / 60);
+    myTime.seconds += Math.floor(Math.floor(tempSeconds % 3600) % 60);
+    console.log(
+      `Переданий час (через зміну секунд) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
+    );
+  } else {
+    console.log('Ви ввели проміжок часу більше, ніж 24 години');
+  }
+};
+myTime.showChangeSecond();
 
 // Задача 3 - Функція, що перераховує базовий час, якщо я ввів хвилини
-// Код великий, тому що я вирішив додати трохи краси у вигляді "0"
 myTime.showChangeMinutes = () => {
   let tempMinutes = Math.abs(
     +prompt('Введіть будь-яку кількість хвилин числом')
@@ -158,82 +129,223 @@ myTime.showChangeMinutes = () => {
 
   if (tempMinutes > 0 && tempMinutes < 60) {
     myTime.minutes += tempMinutes;
-    if (myTime.hours == 0 || myTime.hours < 10) {
-      myTime.hours = String(0) + myTime.hours;
-    }
-    if (myTime.minutes < 10) {
-      myTime.minutes = String(0) + myTime.minutes;
-    }
-    if (myTime.seconds == 0 || myTime.seconds < 10) {
-      myTime.seconds = String(0) + myTime.seconds;
-    }
     console.log(
-      `Переданий час: ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
+      `Переданий час (через зміну хвилин) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
     );
   } else if (tempMinutes >= 60 && tempMinutes <= 1440) {
     myTime.hours += Math.floor(tempMinutes / 60);
     myTime.minutes += Math.floor(tempMinutes % 60);
     myTime.seconds += Math.floor(((tempMinutes % 60) * 60) / 60);
-    if (myTime.hours == 0 || myTime.hours < 10) {
-      myTime.hours = String(0) + myTime.hours;
-    }
-    if (myTime.minutes == 0 || myTime.minutes < 10) {
-      myTime.minutes = String(0) + myTime.minutes;
-    }
-    if (myTime.seconds == 0 || myTime.seconds < 10) {
-      myTime.seconds = String(0) + myTime.seconds;
-    }
     console.log(
-      `Переданий час (через зміну хвилин): ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
+      `Переданий час (через зміну хвилин) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
     );
   } else {
     console.log('Ви ввели проміжок часу більше, ніж 24 години');
   }
 };
-// myTime.showChangeMinutes();
+myTime.showChangeMinutes();
 
 // Задача 4 - Функція, що перераховує базовий час, якщо я ввів години
-// Код великий, тому що я вирішив додати трохи краси у вигляді "0"
 myTime.showChangeHours = () => {
   let tempHours = Math.abs(+prompt('Введіть будь-яку кількість годин числом'));
 
-  if (tempHours > 0 && tempHours <= 24) {
+  if (tempHours >= 0 && tempHours <= 24) {
     myTime.hours += Math.floor(tempHours);
-    myTime.minutes += Math.floor((tempHours - myTime.hours) * 60);
+
+    myTime.minutes += Math.floor(tempHours * 60 - Math.floor(tempHours) * 60);
+    if (myTime.minutes >= 60) {
+      myTime.hours += Math.floor(myTime.minutes / 60);
+      myTime.minutes += Math.floor(myTime.minutes % 60);
+    }
+
     myTime.seconds += Math.floor(
-      ((tempHours - myTime.hours) * 60 - myTime.minutes) * 60
+      tempHours * 60 -
+        Math.floor(tempHours) * 60 -
+        Math.floor(tempHours * 60 - Math.floor(tempHours) * 60)
     );
-    if (myTime.hours < 10) {
-      myTime.hours = String(0) + myTime.hours;
+    if (myTime.seconds >= 60) {
+      myTime.minutes += Math.floor(myTime.seconds / 60);
+      myTime.seconds += Math.floor(myTime.seconds % 60);
     }
-    if (myTime.minutes < 10) {
-      myTime.minutes = String(0) + myTime.minutes;
-    }
-    if (myTime.seconds == 0 || myTime.seconds < 10) {
-      myTime.seconds = String(0) + myTime.seconds;
-    }
+
     console.log(
-      `Переданий час (через зміну годин): ${myTime.hours}:${myTime.minutes}:${myTime.seconds}`
+      `Переданий час (через зміну годин) - годин: ${myTime.hours}, хвилин: ${myTime.minutes}, секунд: ${myTime.seconds}`
     );
   } else {
     console.log('Ви ввели проміжок часу більше, ніж 24 години');
   }
 };
-// myTime.showChangeHours();
+myTime.showChangeHours();
 
-let taskNorma = prompt(
-  'Щоб побачити рішення ДЗ:НОРМА, введи №: 1 (функція показує базовий час), 2(зміна часу через введення секунд), 3(зміна часу через введення хвилин) або 4(зміна часу через введення годин). Дивись в консоль'
-);
-if (taskNorma == '1') {
-  myTime.showBaseTime();
-} else if (taskNorma == '2') {
-  myTime.showChangeSecond();
-} else if (taskNorma == '3') {
-  myTime.showChangeMinutes();
-} else if (taskNorma == '4') {
-  myTime.showChangeHours();
-} else {
-  console.log(
-    'Якщо ти ввів інше значення, то тобі не цікаво перевіряти моє дз)))'
-  );
-}
+// ?--------------------ДОМАШНЕЄ ЗАВДАННЯ: МАКСИМУМ----------------------
+let fractionFirst = {
+  x: +prompt(`Перший дріб. Чисельник - введіть будь-яке число цифрою`),
+  y: +prompt(`Перший дріб. Знаменник - введіть будь-яке число цифрою`),
+};
+
+let fractionSecond = {
+  x: +prompt(`Другий дріб. Чисельник - введіть будь-яке число цифрою`),
+  y: +prompt(`Другий дріб. Знаменник - введіть будь-яке число цифрою`),
+};
+
+let fractionMath = {
+  x: null,
+  y: null,
+  fractionSum() {
+    if (
+      fractionFirst.x !== ' ' &&
+      fractionFirst.y !== ' ' &&
+      fractionSecond.x !== ' ' &&
+      fractionSecond.y !== ' ' &&
+      fractionFirst.x !== 0 &&
+      fractionFirst.y !== 0 &&
+      fractionSecond.x !== 0 &&
+      fractionSecond.y !== 0 &&
+      fractionFirst.y == fractionSecond.y
+    ) {
+      fractionMath.x = fractionFirst.x + fractionSecond.x;
+      fractionMath.y = fractionFirst.y;
+      console.log(
+        `В результаті додавання двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else if (fractionFirst.y !== fractionSecond.y) {
+      fractionMath.x =
+        fractionFirst.x * fractionSecond.y + fractionSecond.x * fractionFirst.y;
+      fractionMath.y = fractionFirst.y * fractionSecond.y;
+      console.log(
+        `В результаті додавання двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else {
+      console.log(
+        'Ви ввели не коректні значення, а тому сталася помилка при додаванні дробів'
+      );
+    }
+  },
+
+  fractionSubtraction() {
+    if (
+      fractionFirst.x !== ' ' &&
+      fractionFirst.y !== ' ' &&
+      fractionSecond.x !== ' ' &&
+      fractionSecond.y !== ' ' &&
+      fractionFirst.x !== 0 &&
+      fractionFirst.y !== 0 &&
+      fractionSecond.x !== 0 &&
+      fractionSecond.y !== 0 &&
+      fractionFirst.y == fractionSecond.y
+    ) {
+      fractionMath.x = fractionFirst.x - fractionSecond.x;
+      fractionMath.y = fractionFirst.y;
+      console.log(
+        `В результаті віднімання двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else if (fractionFirst.y !== fractionSecond.y) {
+      fractionMath.x =
+        fractionFirst.x * fractionSecond.y - fractionSecond.x * fractionFirst.y;
+      fractionMath.y = fractionFirst.y * fractionSecond.y;
+      console.log(
+        `В результаті віднімання двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else {
+      console.log(
+        'Ви ввели не коректні значення, а тому сталася помилка при відніманні дробів'
+      );
+    }
+  },
+  fractionMultiplication() {
+    if (
+      fractionFirst.x !== 0 &&
+      fractionFirst.y !== 0 &&
+      fractionSecond.x !== 0 &&
+      fractionSecond.y !== 0
+    ) {
+      fractionMath.x = fractionFirst.x * fractionSecond.x;
+      fractionMath.y = fractionFirst.y * fractionSecond.y;
+      console.log(
+        `В результаті множення двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else {
+      console.log(
+        'Ви ввели не коректні значення, а тому сталася помилка при множенні дробів'
+      );
+    }
+  },
+
+  fractionDivision() {
+    if (
+      fractionFirst.x !== 0 &&
+      fractionFirst.y !== 0 &&
+      fractionSecond.x !== 0 &&
+      fractionSecond.y !== 0 &&
+      fractionFirst.x == fractionSecond.x &&
+      fractionFirst.y == fractionSecond.y
+    ) {
+      fractionMath.x = 1;
+      fractionMath.y = 1;
+      console.log(
+        `В результаті ділення двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else if (
+      fractionFirst.x !== 0 &&
+      fractionFirst.y !== 0 &&
+      fractionSecond.x !== 0 &&
+      fractionSecond.y !== 0 &&
+      fractionFirst.x !== fractionSecond.x &&
+      fractionFirst.y !== fractionSecond.y
+    ) {
+      fractionMath.x = fractionFirst.x * fractionSecond.y;
+      fractionMath.y = fractionFirst.y * fractionSecond.x;
+      console.log(
+        `В результаті ділення двох дробів ${fractionFirst.x}/${fractionFirst.y} та ${fractionSecond.x}/${fractionSecond.y} отримано новий дріб ${fractionMath.x}/${fractionMath.y}`
+      );
+    } else {
+      console.log(
+        'Ви ввели не коректні значення, а тому сталася помилка при діленні дробів'
+      );
+    }
+  },
+
+  fractionReduction() {
+    if (fractionFirst.y % fractionFirst.x == 0) {
+      fractionFirst.y = fractionFirst.y / fractionFirst.x;
+      fractionFirst.x = 1;
+      console.log(
+        `В результаті скорочення отримано новий дріб ${fractionFirst.x}/${fractionFirst.y}`
+      );
+    } else if (fractionFirst.x % 2 == 0 && fractionFirst.y % 2 == 0) {
+      fractionFirst.x = fractionFirst.x / 2;
+      fractionFirst.y = fractionFirst.y / 2;
+      console.log(
+        `В результаті скорочення отримано новий дріб ${fractionFirst.x}/${fractionFirst.y}`
+      );
+    } else if (fractionFirst.x % 3 == 0 && fractionFirst.y % 3 == 0) {
+      fractionFirst.x = fractionFirst.x / 3;
+      fractionFirst.y = fractionFirst.y / 3;
+      console.log(
+        `В результаті скорочення отримано новий дріб ${fractionFirst.x}/${fractionFirst.y}`
+      );
+    } else if (fractionFirst.x % 5 == 0 && fractionFirst.y % 5 == 0) {
+      fractionFirst.x = fractionFirst.x / 5;
+      fractionFirst.y = fractionFirst.y / 5;
+      console.log(
+        `В результаті скорочення отримано новий дріб ${fractionFirst.x}/${fractionFirst.y}`
+      );
+    } else if (fractionFirst.x % 7 == 0 && fractionFirst.y % 7 == 0) {
+      fractionFirst.x = fractionFirst.x / 7;
+      fractionFirst.y = fractionFirst.y / 7;
+      console.log(
+        `В результаті скорочення отримано новий дріб ${fractionFirst.x}/${fractionFirst.y}`
+      );
+    } else {
+      console.log(
+        `Даний дріб ${fractionFirst.x}/${fractionFirst.y} не скорочується`
+      );
+    }
+  },
+};
+
+fractionMath.fractionSum();
+fractionMath.fractionSubtraction();
+fractionMath.fractionMultiplication();
+fractionMath.fractionDivision();
+fractionMath.fractionReduction();
